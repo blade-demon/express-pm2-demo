c1a2261c4055f77601b8e5ec45dbe49beab2a385
blade-demon <949659359@qq.com>
2017-12-16 00:26:57 +0800

    fix bugs

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 ssh jenkins@ec2-13-125-117-151.ap-northeast-2.compute.amazonaws.com
- cd /home/ubuntu/express-pm2-demo
- git pull
- npm install
- pm2 restart all
- exit
+cd /home/ubuntu/express-pm2-demo
+git pull
+npm install
+pm2 restart all
